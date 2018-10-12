086084b548524982b96a60534ae7e842605cd928
Xiang Gang <1134002741@qq.com>
2018-10-12 17:58:06 +0800

    tow

--- js/psIndex.js
+++ js/psIndex.js
@@ -1,29 +1,28 @@
-//header 头部
-(function(){
-	let aLi = document.querySelectorAll("#header .cen-right ul li"),
-		aDiv = document.querySelectorAll("#header .cen-right ul li div"),
-		aTxt = document.querySelectorAll("#header .cen-right ul li div textarea")[0],
-		aBtn =document.querySelectorAll("#header .cen-right ul li div button")[0],
-		index = 0,
-		len = aLi.length;
-	for(let i=0; i<len; i++){
-		aLi[i].onmouseenter = function(){
-			aDiv[index].classList.remove("active");
-			aDiv[i].classList.add("active");
-			index = i;	
-		}
-		aLi[i].onmouseleave = function(){
-			if(i!==2){
-				aDiv[i].classList.remove("active");
-			}	
-		}
-	}
-		aBtn.onclick = function(){
-			if(aTxt.value === ""){
-				aTxt.value += "请输入内容";
-			}else{
-				aTxt.value = "";
-				aDiv[2].classList.remove("active");
-			}
-		}
-})();
+// (function () {
+// 	let aLi = document.querySelectorAll("#content .p-c-btn .list li"),
+// 		aDiv = document.querySelectorAll("#content .p-c-btn .list li div") ,
+// 		actSpan = document.querySelector("#content .p-c-btn .list li div.active p span"),
+// 		onSpan = document.querySelector("#content .p-c-btn .list li div.on p span"),
+// 		len = aLi.length;
+// 	for(let i=0;i<len;i++){
+		
+// 		aLi[i].onclick = function(){
+// 			for(let j=0;j<aDiv.length;j++){
+// 				aDiv[j].classList.remove("active");
+// 				j++;
+// 				aDiv[j].classList.add("active");
+// 				console.log(j);
+// 			}
+			
+// 			console.log(i);
+// 		}
+// 	}
+// 	// actSpan.onclick =function(){
+// 	// 	aDiv[0].classList.remove("active");
+// 	// 	aDiv[1].classList.add("active");
+// 	// }
+// 	// onSpan.onclick =function(){
+// 	// 	aDiv[0].classList.add("active");
+// 	// 	aDiv[1].classList.remove("active");
+// 	// }
+// })();

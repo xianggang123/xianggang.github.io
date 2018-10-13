47097aa95299ab7987f57a402299332f47a3e61e
Yang Hu <hyytez@gmail.com>
2018-10-13 08:29:49 +0800

    将PS页面的鼠标移动事件修改为点击事件；取消了PS页面的图片高度限制

--- js/psIndex.js
+++ js/psIndex.js
@@ -1,28 +1,42 @@
-// (function () {
-// 	let aLi = document.querySelectorAll("#content .p-c-btn .list li"),
-// 		aDiv = document.querySelectorAll("#content .p-c-btn .list li div") ,
-// 		actSpan = document.querySelector("#content .p-c-btn .list li div.active p span"),
-// 		onSpan = document.querySelector("#content .p-c-btn .list li div.on p span"),
-// 		len = aLi.length;
-// 	for(let i=0;i<len;i++){
-		
-// 		aLi[i].onclick = function(){
-// 			for(let j=0;j<aDiv.length;j++){
-// 				aDiv[j].classList.remove("active");
-// 				j++;
-// 				aDiv[j].classList.add("active");
-// 				console.log(j);
-// 			}
-			
-// 			console.log(i);
-// 		}
-// 	}
-// 	// actSpan.onclick =function(){
-// 	// 	aDiv[0].classList.remove("active");
-// 	// 	aDiv[1].classList.add("active");
-// 	// }
-// 	// onSpan.onclick =function(){
-// 	// 	aDiv[0].classList.add("active");
-// 	// 	aDiv[1].classList.remove("active");
-// 	// }
-// })();
+document.addEventListener("click", (event) => {
+    console.log(event.target.nextElementSibling)
+});
+
+const figureList = document.querySelector(".list");
+
+
+//figureList.addEventListener("click", (event) => {
+//    //    console.log(event.target.src);
+//    if (event.target.tagName == "IMG") {
+//        let src = event.target.src.substring(0, 35);
+//        let caption = event.target.nextElementSibling;
+//        if (event.target.src.slice(-3) == "jpg") {
+//            event.target.src = src + "png";
+//            caption.textContent = "效果图";
+//        } else if (event.target.src.slice(-3) == "png") {
+//            event.target.src = src + "jpg";
+//            caption.textContent = "原图";
+//        }
+//    }
+//
+//});
+
+
+figureList.addEventListener("click", (event) => {
+    if (event.target.tagName == "IMG") {
+        let src = event.target.src.split(".");
+        let caption = event.target.nextElementSibling;
+        if (event.target.src.slice(-3) == "jpg") {
+            src[src.length - 1] = "png"
+            let output = src.join(".")
+            event.target.src = output
+            caption.textContent = "效果图";
+        } else if (event.target.src.slice(-3) == "png") {
+            src[src.length - 1] = "jpg"
+            let output = src.join(".")
+            event.target.src = output
+            caption.textContent = "原图";
+        }
+    }
+
+});
